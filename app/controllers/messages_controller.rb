589e19521500f26dd97a07831d9bfc22d3622bb5
shot9269 <110fe603@gmail.com>
2020-10-17 13:32:58 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,22 @@
 class MessagesController < ApplicationController
   def index
-    
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+  end
+
+  def create
+    # message = Message.new(message_params)
+    # message.save
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to room_messages_path
+    else
+      render :index
+    end
+  end
+private
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
