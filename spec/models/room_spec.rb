076b8328ad9294d28ca5962734f5051fd5f9fb84
shot9269 <110fe603@gmail.com>
2020-10-18 10:36:50 +0900

    roomとmessageのモデルテスト

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
+  describe "#create" do
+    before do
+      @room = FactoryBot.build(:room)
+    end
+
+    it "nameの値が存在する" do
+      expect(@room).to be_valid
+    end
+    it "nameの値が空だと保存できない" do
+      @room.name = ""
+      @room.valid?      
+      expect(@room.errors.full_messages).to include("Name can't be blank")
+
+    end
+  end
 end
